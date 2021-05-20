fe1c080506e8b0efb298e6aa8c6eec07c537c1c9
lekpramual <lek.pramual@gmail.com>
2021-05-20 09:26:23 +0700

    Updates

---
+++ precache-manifest.448ea5d332a60b7579dbba13da699eb2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e5155ce56f1c45c2df8818d10e1b7c",
+    "url": "/make/index.html"
+  },
+  {
+    "revision": "77d96ad65c3b7b3acaaf",
+    "url": "/make/static/css/2.a324e314.chunk.css"
+  },
+  {
+    "revision": "77d96ad65c3b7b3acaaf",
+    "url": "/make/static/js/2.e747433f.chunk.js"
+  },
+  {
+    "revision": "6667dc153edcbe95e8723d6c8f6541e2",
+    "url": "/make/static/js/2.e747433f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ee9742ef274c6c39d0",
+    "url": "/make/static/js/3.207db6a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/make/static/js/3.207db6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c4a03915a72aab3dfe",
+    "url": "/make/static/js/4.4ed0b504.chunk.js"
+  },
+  {
+    "revision": "1f065874442d89543f63",
+    "url": "/make/static/js/5.0bb57786.chunk.js"
+  },
+  {
+    "revision": "a12de25f15da78dc65ae",
+    "url": "/make/static/js/6.605ecb73.chunk.js"
+  },
+  {
+    "revision": "b3e6fc1c83345f0a69db",
+    "url": "/make/static/js/7.055a8a58.chunk.js"
+  },
+  {
+    "revision": "6d70978b16800ae923ab",
+    "url": "/make/static/js/main.89891171.chunk.js"
+  },
+  {
+    "revision": "7637688204352dc52917",
+    "url": "/make/static/js/runtime-main.fba1cc7d.js"
+  },
+  {
+    "revision": "94550ee47509a1a29a5076fd493e0926",
+    "url": "/make/static/media/014-calendar.94550ee4.svg"
+  }
+]);
